421bd860a93739c19be311152df9a14b076c64c6
Duc Nguyen <duc.nguyen@codecomplete.jp>
2024-08-02 07:29:38 +0700

    haha

--- app/Models/Char.php
+++ app/Models/Char.php
@@ -122,6 +122,6 @@ class Char extends Model
     }
 
     public function user() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, "userid", "userid");
     }
 }
